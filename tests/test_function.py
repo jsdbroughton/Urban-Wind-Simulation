5133a31ae7f68d3f1af9089fe322628ad59bba31
Jonathon Broughton <jonathon@stardotbmp.com>
2025-02-13 00:40:36 +0000

    Update test function inputs for automation run
    - Changed input parameters for the automation function.
    - Removed forbidden speckle type and whisper message.
    - Added wind speed, wind direction, and number of CPUs.

--- tests/test_function.py
+++ tests/test_function.py
@@ -22,10 +22,7 @@ def test_function_run(test_automation_run_data: AutomationRunData, test_automati
     automate_sdk = run_function(
         automation_context,
         automate_function,
-        FunctionInputs(
-            forbidden_speckle_type="None",
-            whisper_message=SecretStr("testing automatically"),
-        ),
+        FunctionInputs(wind_speed=10, wind_direction=90, number_of_cpus=6)
     )
 
     assert automate_sdk.run_status == AutomationStatus.SUCCEEDED
